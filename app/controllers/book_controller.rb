f597cb287bb2e9632fd25df0794d88c71296bae6
lanibon <nguyenlan13@gmail.com>
2019-08-17 00:52:59 -0700

    fix duplicate logic and :id route

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,44 +1,59 @@
 class BookController < ApplicationController
 
   
+  get '/books' do
+    authenticate
+    @books = Book.all
+    erb :'book/index'
+  end
+  
+  
+  
   get '/books/new' do
     authenticate
     @books = Book.all
     erb :'book/new'
   end
 
-  post '/books/new' do
+  post '/books' do
     authenticate
     book_params = params[:book]
     author = Author.find_or_create_by(name: book_params[:author])
     book = Book.find_by(title: book_params[:title], year: book_params[:year], author: author)
-  #  binding.pry
+    # binding.pry
     if book
-      #need to fix this so it doesnt show up everytime
       @duplicate = true
       @books = Book.all
       erb :'book/new'
     else
+      @duplicate = false
       book = Book.create(title: book_params[:title], year: book_params[:year], author: author)
-      redirect '/books/#{book.id}'
+      redirect "/books/#{book.id}"
     end
   end
 
 
   get '/books/:id' do
     authenticate
-    book = book.id
 
+    @book = Book.find_by(id: params[:id])
+    if !@book
+      redirect "/books"
+      return
+    end
+    @author = Author.find_by(id: @book.author.id)
 
-
-    # erb :'book/show'
-    
+    #book = book.id
+    erb :'book/show'
   end
 
   post '/books/:id/comments' do
     user = current_user
-    @comment = Comment.new(user: user, content: params[:content], commentable: params[:commentable])
+    @comment = Comment.create(user: user, content: params[:content], commentable: params[:commentable])
 
-    erb :'book/comments'
+    erb :'comments/new'
   end
+
+
+
 end
