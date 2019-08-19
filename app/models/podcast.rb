213d866b1f8aca920185cb8d0a8f040c51579643
lanibon <nguyenlan13@gmail.com>
2019-08-19 02:42:08 -0700

    add podcast validations

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,4 +1,9 @@
 class Podcast < ActiveRecord::Base
+  validates :name, uniqueness: true, presence: true
+  validates :episode_title, uniqueness: true, presence: true
+  validates :year, presence: true
+  validates :source, presence: true
+
   belongs_to :author
   has_many :comments, as: :commentable
   has_many :ratings, as: :ratable
