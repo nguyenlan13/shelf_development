5c0b5e1670282304f3656c3cd597a9b2b328c3d0
lanibon <nguyenlan13@gmail.com>
2019-08-23 00:30:17 -0700

    add crud to fave_quotes

--- app/controllers/fave_quote_controller.rb
+++ app/controllers/fave_quote_controller.rb
@@ -10,7 +10,57 @@ class FaveQuoteController < ApplicationController
     @quote_path = params[:fave_quote][:quote_path]
     FaveQuote.create(user: user, quote: params[:fave_quote][:quote], quotable_id: @quotable_id, quotable_type: @quotable_type)
     redirect "/#{@quote_path}/#{@quotable_id}"
-    
 	end
+
+
+  get '/fave_quotes/:id/edit' do
+    authorize
+    @fave_quote = FaveQuote.find_by(id: params[:id])
+    if @fave_quote.user_id != current_user.id
+      redirect '/access_denied'    
+    end
+    erb :"fave_quote/edit"
+  end
+
+
+  patch '/fave_quotes/:id' do
+    authorize
+    fq_path
+
+    @fave_quote = FaveQuote.find_by(id: params[:id])
+    if @fave_quote.user_id != current_user.id
+      redirect '/access_denied'      
+      else
+        # params[:comment][:content] = params[:comment][:content]
+       @fave_quote.update(quote: params[:fave_quote][:quote])
+      end
+        redirect "/#{@quote_path}/#{@quotable_id}"
+  end
+
+
+  delete '/fave_quotes/:id/delete' do
+    authorize
+    fq_path
+
+    @fave_quote = FaveQuote.find_by(id: params[:id])
+    if @fave_quote.user_id != current_user.id
+      redirect '/access_denied'      
+      else
+        @fave_quote.delete
+        redirect "/#{@quote_path}/#{@quotable_id}"
+      end
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
 
 end
