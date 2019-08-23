8930339fbf7f2706da2475fc1bf503e0e6ae02d5
lanibon <nguyenlan13@gmail.com>
2019-08-23 00:29:38 -0700

    add crud to comments

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -7,45 +7,47 @@ class CommentController < ApplicationController
     user = current_user
     @commentable_id = params[:comment][:commentable_id]
     @commentable_type = params[:comment][:commentable_type]
-    # @comment_path = params[:comment][:comment_path]
+    @comment_path = params[:comment][:comment_path]
     Comment.create(user: user, content: params[:comment][:content], commentable_id: @commentable_id, commentable_type: @commentable_type)
-   
-    redirect "/#{@commentable_type}s/#{@commentable_id}"
+    redirect "/#{@comment_path}/#{@commentable_id}"
 	end
+
 
   get '/comments/:id/edit' do
     authorize
     @comment = Comment.find_by(id: params[:id])
     if @comment.user_id != current_user.id
-      redirect '/access-denied'    
+      redirect '/access_denied'    
     end
     erb :"comment/edit"
   end
 
+
   patch '/comments/:id' do
     authorize
-    @commentable_id = params[:comment][:commentable_id]
-    @commentable_type = params[:comment][:commentable_type]
-    @comment_path = params[:comment][:comment_path]
+    comm_path
+
     @comment = Comment.find_by(id: params[:id])
       if @comment.user_id != current_user.id
-        redirect '/access-denied'    
+        redirect '/access_denied'     
+      else
+        # params[:comment][:content] = params[:comment][:content]
+       @comment.update(content: params[:comment][:content])
       end
-        params[:comment][:content] = params[:comment][:content]
-        @comment.update(params[:comment])
         redirect "/#{@comment_path}/#{@commentable_id}"
   end
 
+
   delete '/comments/:id/delete' do
     authorize
-    @commentable_id = params[:comment][:commentable_id]
-    @commentable_type = params[:comment][:commentable_type]
-    @comment_path = params[:comment][:comment_path]
+    comm_path
+
     @comment = Comment.find_by(id: params[:id])
       if @comment.user_id != current_user.id
-        redirect '/access-denied'    
-      end
+        redirect '/access_denied'    
+      else
         @comment.delete
         redirect "/#{@comment_path}/#{@commentable_id}"
-  end
+      end
+    end
 end
